60c4009e8301488047df73ce0bed528daa1ec140
Marcell Toth <marcell@marcelltoth.net>
2020-03-17 18:04:35 +0200

    fix Github Release action

--- .changeset/getChangelogEntry.js
+++ .changeset/getChangelogEntry.js
@@ -6,7 +6,7 @@ const getReleaseLine = async (changeset, type) => {
     .split('\n')
     .map(l => l.trimRight());
   let { links } = await getInfo({
-    repo: 'JedWatson/react-select',
+    repo: 'marcelltoth/react-select-reborn',
     commit: changeset.commit,
   });
   return `- ${links.commit}${links.pull === null ? '' : ` ${links.pull}`}${
